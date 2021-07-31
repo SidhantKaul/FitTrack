52ab0730ece36b9e385dde26e20d4f84fb3b5ea7
ayushhh234 <sidhantkaul10@gmail.com>
2021-07-31 11:12:46 +0530

    Login bug4

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://boiling-caverns-16015.herokuapp.com/fitness",
+    callbackURL: "https://boiling-caverns-16015.herokuapp.com/auth/google/fitness",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
